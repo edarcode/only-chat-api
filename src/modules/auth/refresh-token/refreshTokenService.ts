38f308640adaade276ff54569c57c8104b03cc71
Edwin Ortiz <edarcode@gmail.com>
2024-08-15 17:00:41 -0500

    refresh token funcional

---
+++ src/modules/auth/refresh-token/refreshTokenService.ts
@@ -0,0 +1,16 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT } from "../../../constants/jwt";
+
+export const refreshTokenService = (token: string) => {
+  const tokenInfo = jwt.decode(token) as JwtPayload;
+
+  const newTokenInfo = { ...tokenInfo };
+  if (newTokenInfo.iat) delete newTokenInfo.iat;
+  if (newTokenInfo.exp) delete newTokenInfo.exp;
+
+  const newToken = jwt.sign(newTokenInfo, JWT.secret as string, {
+    expiresIn: JWT.expiresIn,
+  });
+
+  return newToken;
+};
