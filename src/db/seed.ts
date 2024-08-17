da9d13706a4b49acb66bebeee26bed3eeed8bcd2
Edwin Ortiz <edarcode@gmail.com>
2024-08-16 20:27:53 -0500

    agg seed de la tabla follows

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,10 +1,11 @@
 import "../utils/dotenv";
 import { BCRYPT } from "../constants/bcrypt";
 import { db } from "./db";
-import { ROLE, usersTable } from "./schema";
+import { followsTable, ROLE, usersTable } from "./schema";
 import bcrypt from "bcrypt";
 
-const EDARCODE = {
+const TEST = {
+  id: crypto.randomUUID(),
   username: process.env.CHIEF_USERNAME as string,
   email: process.env.CHIEF_EMAIL as string,
   password: process.env.CHIEF_PASSWORD as string,
@@ -12,6 +13,7 @@ const EDARCODE = {
 };
 
 const LORE = {
+  id: crypto.randomUUID(),
   username: "lore",
   email: "lore@gmail.com",
   password: "123456",
@@ -19,11 +21,15 @@ const LORE = {
 };
 
 const seedUsers = async () => {
-  // await db.delete(usersTable).execute();
-  EDARCODE.password = await bcrypt.hash(EDARCODE.password, BCRYPT.salt);
+  TEST.password = await bcrypt.hash(TEST.password, BCRYPT.salt);
   LORE.password = await bcrypt.hash(LORE.password, BCRYPT.salt);
 
-  await db.insert(usersTable).values([EDARCODE, LORE]);
+  await db.delete(usersTable).execute();
+  await db.insert(usersTable).values([TEST, LORE]);
+
+  await db
+    .insert(followsTable)
+    .values({ followerId: TEST.id, followingId: LORE.id });
 };
 
 seedUsers().catch(console.error);
